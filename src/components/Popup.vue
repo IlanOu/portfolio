3b00b47415b84a73d3693d7e7f1825a74e865210
Ilan <71207643+IlanOu@users.noreply.github.com>
2023-12-11 09:49:32 +0000

    popup menu : - tab images and video - sticky description

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -3,12 +3,31 @@
         <div class="popup-content" @click.stop>
             <div class="date-tag">{{ props.project.date }}</div>
             <h2>{{ props.project.projectName }}</h2>
+    
             <div class="columns">
-                <div class="images-gallery">
-                    <div class="gallery-item" v-for="(imageUrl, index) in props.project.imageUrls" :key="index">
-                        <img :src="imageUrl" alt="Project Image">
+                <div class="galleries">
+                    <!-- Ajout des boutons Images / Videos -->
+                    <div class="buttons">
+                        <button @click="showImages" :class="{ active: displayImages }" class="button">Images</button>
+                        <button @click="showVideos" :class="{ active: displayVideos, disabled: videoListEmpty }" class="button">Vidéos</button>
+                    </div>
+    
+                    <!-- Videos au-dessus des images -->
+                    <div v-if="displayVideos" class="videos-gallery">
+                        <div class="video-item" v-for="(videoEmbed, index) in props.project.videoUrls" :key="'video-' + index">
+                            <iframe :src="videoEmbed" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <!-- Galerie d'images à droite des vidéos -->
+                    <div v-if="displayImages" class="images-gallery">
+                        <div class="gallery-item" v-for="(imageUrl, index) in props.project.imageUrls" :key="index">
+                            <img :src="imageUrl" alt="Project Image">
+                        </div>
                     </div>
                 </div>
+    
+                <!-- Description à droite des images -->
                 <div class="description angles-with-padding">
                     <p>{{ props.project.description }}</p>
                 </div>
@@ -16,100 +35,181 @@
         </div>
     </div>
 </template>
-  
+
 <script setup>
-    import {
-        defineProps,
-        defineEmits,
-        onMounted
-    } from 'vue';
-    const props = defineProps(['project']);
-    const emits = defineEmits(['closePopup']);
-    const closePopup = () => {
-        emits('closePopup');
-    };
-    onMounted(() => {
-        const imagesGallery = document.querySelector(".images-gallery");
-        if (imagesGallery) {
-            if (props.project.imageUrls.length < 3) {
-                imagesGallery.style.columnCount = props.project.imageUrls.length;
-            }
-        }
-    });
+import { defineProps, defineEmits, ref } from 'vue';
+
+const props = defineProps(['project']);
+const emits = defineEmits(['closePopup']);
+
+const closePopup = () => {
+    emits('closePopup');
+};
+
+// Ajout de la logique pour afficher les images ou les vidéos
+const displayImages = ref(true);
+const displayVideos = ref(false);
+const videoListEmpty = ref(props.project.videoUrls.length === 0);
+
+const showImages = () => {
+    displayImages.value = true;
+    displayVideos.value = false;
+};
+
+const showVideos = () => {
+    displayImages.value = false;
+    displayVideos.value = true;
+};
+
 </script>
-  
+
 <style scoped>
-    h2 {
-        margin-bottom: 1rem;
-        text-decoration: underline;
-    }
-    .popup-overlay {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: rgba(0, 0, 0, 0.7);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .popup-content {
-        background-color: var(--color-background-soft);
-        color: var(--color-text);
-        padding: 1rem;
-        border-radius: 1.5rem;
-        border: solid 1px var(--color-border);
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-        max-width: 80%;
-        text-align: center;
-        width: 80%;
-        height: 80%;
-        overflow: hidden visible;
-        position: relative;
-    }
-    .gallery-item img {
-        width: 100%;
-        height: auto;
-        border-radius: 8px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-    .description {
-        position: relative;
-        width: 30%;
-        text-align: justify;
-        height: fit-content;
-    }
-    .date-tag {
-        position: absolute;
-        top: 0;
-        right: 0;
-        margin: 0.5rem;
-        background-color: var(--color-accentuation);
-        padding: 0.25rem 0.5rem;
-        border-radius: 0.5rem;
-        color: var(--color-text); /* Utilisez la couleur du texte définie dans votre thème */
-        font-size: 0.8rem;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-    }
+h2 {
+    margin-bottom: 1rem;
+    text-decoration: underline;
+}
+
+.popup-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.popup-content {
+    background-color: var(--color-background-soft);
+    color: var(--color-text);
+    padding: 1rem;
+    border-radius: 1.5rem;
+    border: solid 1px var(--color-border);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+    max-width: 80%;
+    text-align: center;
+    width: 80%;
+    height: 80%;
+    overflow: hidden visible;
+    position: relative;
+}
+
+.columns {
+    display: flex;
+    justify-content: space-evenly;
+}
+
+.galleries {
+    width: 65%;
+}
+
+.buttons{
+    display: flex;
+    justify-content: space-around;
+}
+
+.button{
+    width: 40%;
+    margin-bottom: 3rem;
+    background-color: var(--color-accentuation-variant) ;
+    padding: 0.75rem 0.5rem;
+    border-radius: 0.5rem;
+    border: 0px;
+    color:  var(--color-text) ;
+    font-size: 0.8rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+
+    cursor: pointer;
+}
+
+.button.active {
+    background-color: var(--color-accentuation); /* Couleur active */
+    color: var(--color-text); /* Couleur du texte active */
+}
+
+.button.disabled {
+    background-color: var(--color-disabled); /* Couleur pour le bouton désactivé */
+    cursor: not-allowed;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   Videos                                   */
+/* -------------------------------------------------------------------------- */
+
+.videos-gallery {
+    column-count: 2;
+}
+
+.video-item iframe {
+    width: 100%;
+    aspect-ratio: 16 / 9;
+    height: auto;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   Images                                   */
+/* -------------------------------------------------------------------------- */
+
+.images-gallery {
+    column-count: 3;
+    max-height: 100%;
+   
+    max-height: calc(100% - 2rem); /* Ajuste la hauteur maximale de la galerie d'images */
+    overflow-y: auto;
+}
+
+.gallery-item img {
+    width: 100%;
+    height: auto;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 Description                                */
+/* -------------------------------------------------------------------------- */
+
+.description {
+    position: sticky;
+    top: 1rem;
+    /* position: relative; */
+    width: 30%;
+    text-align: justify;
+    height: fit-content;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    Date                                    */
+/* -------------------------------------------------------------------------- */
+
+.date-tag {
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 0.5rem;
+    background-color: var(--color-accentuation);
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.5rem;
+    color: var(--color-text);
+    /* Utilisez la couleur du texte définie dans votre thème */
+    font-size: 0.8rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+@media (max-width: 1024px) {
     .columns {
         display: flex;
+        flex-direction: column-reverse;
         justify-content: space-evenly;
+        align-items: center;
     }
     .images-gallery {
         width: 65%;
-        column-count: 3;
+        column-count: 1;
     }
-    @media (max-width: 1024px) {
-        .columns {
-            display: flex;
-            flex-direction: column-reverse;
-            justify-content: space-evenly;
-            align-items: center;
-        }
-        .images-gallery {
-            width: 65%;
-            column-count: 1;
-        }
-    }
+}
 </style>
