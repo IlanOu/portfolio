9f74c33918f3ab5aa1c57aea731c6eba4a1cb23a
Ilan Outhier <71207643+IlanOu@users.noreply.github.com>
2024-09-12 12:34:42 +0200

    WIP : add lab test

--- src/utils/content-utils.ts
+++ src/utils/content-utils.ts
@@ -4,8 +4,30 @@ import { getCollection } from 'astro:content'
 
 export async function getSortedPosts() {
   const allBlogPosts = await getCollection('posts', ({ data }) => {
-    return import.meta.env.PROD ? data.draft !== true : true
+    return import.meta.env.PROD ? data.draft !== true && data.lab !== true : true;
+  });
+  const sorted = allBlogPosts.sort((a, b) => {
+    const dateA = new Date(a.data.published)
+    const dateB = new Date(b.data.published)
+    return dateA > dateB ? -1 : 1
   })
+
+  for (let i = 1; i < sorted.length; i++) {
+    sorted[i].data.nextSlug = sorted[i - 1].slug
+    sorted[i].data.nextTitle = sorted[i - 1].data.title
+  }
+  for (let i = 0; i < sorted.length - 1; i++) {
+    sorted[i].data.prevSlug = sorted[i + 1].slug
+    sorted[i].data.prevTitle = sorted[i + 1].data.title
+  }
+
+  return sorted
+}
+
+export async function getSortedLabPosts() {
+  const allBlogPosts = await getCollection('posts', ({ data }) => {
+    return import.meta.env.PROD ? data.lab === true : true;
+  });
   const sorted = allBlogPosts.sort((a, b) => {
     const dateA = new Date(a.data.published)
     const dateB = new Date(b.data.published)
@@ -31,7 +53,7 @@ export type Tag = {
 
 export async function getTagList(): Promise<Tag[]> {
   const allBlogPosts = await getCollection('posts', ({ data }) => {
-    return import.meta.env.PROD ? data.draft !== true : true
+    return import.meta.env.PROD ? data.draft !== true && data.lab !== true : true
   })
 
   const countMap: { [key: string]: number } = {}
@@ -57,7 +79,7 @@ export type Category = {
 
 export async function getCategoryList(): Promise<Category[]> {
   const allBlogPosts = await getCollection('posts', ({ data }) => {
-    return import.meta.env.PROD ? data.draft !== true : true
+    return import.meta.env.PROD ? data.draft !== true && data.lab !== true : true
   })
   const count: { [key: string]: number } = {}
   allBlogPosts.map(post => {
